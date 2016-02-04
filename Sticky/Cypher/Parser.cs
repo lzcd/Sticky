da7b8efd65cc2cd0316dc83cbb2c55db90ec0e2b
lzcd <lzcd@hotmail.com>
2016-02-04 18:57:20 +1100

    initial commit

---
+++ Sticky/Cypher/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sticky.Cypher
+{
+    public class Parser
+    {
+        public void Parse(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
