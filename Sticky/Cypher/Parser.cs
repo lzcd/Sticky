5a8d03161e003ba83edf7fa6062f25621ef7680a
lzcd <lzcd@hotmail.com>
2016-02-05 22:10:57 +1100

    Removing use of static in parsing

--- Sticky/Cypher/Parser.cs
+++ Sticky/Cypher/Parser.cs
@@ -6,100 +6,103 @@ namespace Sticky.Cypher
 {
     public static class Parser
     {
-        static readonly Parser<string> Identifier =
-            from first in Parse.Letter.Once()
-            from rest in Parse.LetterOrDigit.XOr(Parse.Char('-')).XOr(Parse.Char('_')).Many()
-            select new string(first.Concat(rest).ToArray());
 
-        static readonly Parser<char> LabelPrefix = Parse.Char(':');
-
-        static readonly Parser<string> Label =
-            from labelPrefix in LabelPrefix
-            from identifer in Identifier
-            select identifer;
-
-        static readonly Parser<char> SingleQuoteDelimiter = Parse.Char('\'');
-
-        static readonly Parser<string> QuotedText =
-            from openQuote in SingleQuoteDelimiter
-            from text in Parse.AnyChar.Except(SingleQuoteDelimiter).Many()
-            from closeQuote in Parse.Char('\'')
-            select string.Concat(openQuote, new string(text.ToArray()), closeQuote);
-
-        static readonly Parser<string> Number = Parse.Decimal;
-
-        static readonly Parser<char> ValuePrefix = Parse.Char(':');
-
-        static readonly Parser<Property> Property =
-            from leading in Parse.WhiteSpace.Many()
-            from name in Identifier
-            from valuePrefix in ValuePrefix
-            from textValue in Parse.Or(QuotedText, Number)
-            select new Property { Name = name, TextValue = textValue };
-
-        static readonly Parser<IEnumerable<Property>> Properties =
-            from leading in Parse.WhiteSpace.Many()
-            from openBrace in Parse.Char('{')
-            from properties in Property.DelimitedBy(Parse.Char(','))
-            from closeBrace in Parse.Char('}')
-            select properties;
-
-        static readonly Parser<Node> Node =
-            from startNodeChar in Parse.Char('(')
-            from identifier in Identifier
-            from label in Label.Optional()
-            from properties in Properties.Optional()
-            from endNodeChar in Parse.Char(')')
-            select new Node
-            {
-                Identifier = identifier,
-                Label = label.IsDefined ? label.Get() : "",
-                Properties = properties.IsDefined ? properties.Get() : new List<Property>()
-            };
-
-        static readonly Parser<Relationship> Relationship =
-            from leftDirection in Parse.Char('<').Optional()
-            from leftLine in Parse.Char('-')
-            from openBracket in Parse.Char('[')
-            from label in Label
-            from properties in Properties.Optional()
-            from closeBracket in Parse.Char(']')
-            from rightLine in Parse.Char('-')
-            from rightDirection in Parse.Char('>').Optional()
-            select new Relationship
-            {
-                Label = label,
-                Properties = properties.IsDefined ? properties.Get() : new List<Property>(),
-                Direction = leftDirection.IsDefined ?
-                    RelationshipDirection.Left :
-                    (rightDirection.IsDefined ? RelationshipDirection.Right : RelationshipDirection.Unknown)
-            };
-
-        static readonly Parser<Connection> Connection =
-            from relationship in Relationship
-            from rightNode in Node
-            select new Connection { Relationship = relationship, Node = rightNode };
-
-        static readonly Parser<Path> Path =
-            from leading in Parse.WhiteSpace.Many()
-            from leftNode in Node
-            from connection in Connection.Optional()
-            select new Path
-            {
-                Node = leftNode,
-                Connection = connection.IsDefined ? connection.Get() : null
-            };
-
-        static readonly Parser<Command> Command =
-            from leading in Parse.WhiteSpace.Many()
-            from text in Identifier
-            from paths in Path.DelimitedBy(Parse.Char(','))
-            select new Command { Text = text, Paths = paths };
 
         public static void Boop(string source)
         {
-            var command = Command.Parse(source);
-            
+
+            var identifierParser =
+               from first in Parse.Letter.Once()
+               from rest in Parse.LetterOrDigit.XOr(Parse.Char('-')).XOr(Parse.Char('_')).Many()
+               select new string(first.Concat(rest).ToArray());
+
+            var labelPrefixParser = Parse.Char(':');
+
+            var labelParser =
+                from labelPrefix in labelPrefixParser
+                from identifer in identifierParser
+                select identifer;
+
+            var singleQuoteDelimiterParser = Parse.Char('\'');
+
+            var quotedTextParser =
+                from openQuote in singleQuoteDelimiterParser
+                from text in Parse.AnyChar.Except(singleQuoteDelimiterParser).Many()
+                from closeQuote in Parse.Char('\'')
+                select string.Concat(openQuote, new string(text.ToArray()), closeQuote);
+
+            var numberParser = Parse.Decimal;
+
+            var valuePrefixParser = Parse.Char(':');
+
+            var propertyParser =
+                from leading in Parse.WhiteSpace.Many()
+                from name in identifierParser
+                from valuePrefix in valuePrefixParser
+                from textValue in Parse.Or(quotedTextParser, numberParser)
+                select new Property { Name = name, TextValue = textValue };
+
+            var propertiesParser =
+                from leading in Parse.WhiteSpace.Many()
+                from openBrace in Parse.Char('{')
+                from properties in propertyParser.DelimitedBy(Parse.Char(','))
+                from closeBrace in Parse.Char('}')
+                select properties;
+
+            var nodeParser =
+                from startNodeChar in Parse.Char('(')
+                from identifier in identifierParser
+                from label in labelParser.Optional()
+                from properties in propertiesParser.Optional()
+                from endNodeChar in Parse.Char(')')
+                select new Node
+                {
+                    Identifier = identifier,
+                    Label = label.IsDefined ? label.Get() : "",
+                    Properties = properties.IsDefined ? properties.Get() : new List<Property>()
+                };
+
+            var relationshipParser =
+                from leftDirection in Parse.Char('<').Optional()
+                from leftLine in Parse.Char('-')
+                from openBracket in Parse.Char('[')
+                from label in labelParser
+                from properties in propertiesParser.Optional()
+                from closeBracket in Parse.Char(']')
+                from rightLine in Parse.Char('-')
+                from rightDirection in Parse.Char('>').Optional()
+                select new Relationship
+                {
+                    Label = label,
+                    Properties = properties.IsDefined ? properties.Get() : new List<Property>(),
+                    Direction = leftDirection.IsDefined ?
+                        RelationshipDirection.Left :
+                        (rightDirection.IsDefined ? RelationshipDirection.Right : RelationshipDirection.Unknown)
+                };
+
+            var connectionParser =
+                from relationship in relationshipParser
+                from rightNode in nodeParser
+                select new Connection { Relationship = relationship, Node = rightNode };
+
+            var pathParser =
+                from leading in Parse.WhiteSpace.Many()
+                from leftNode in nodeParser
+                from connection in connectionParser.Optional()
+                select new Path
+                {
+                    Node = leftNode,
+                    Connection = connection.IsDefined ? connection.Get() : null
+                };
+
+            var commandParser =
+                from leading in Parse.WhiteSpace.Many()
+                from text in identifierParser
+                from paths in pathParser.DelimitedBy(Parse.Char(','))
+                select new Command { Text = text, Paths = paths };
+
+            var command = commandParser.Parse(source);
+
         }
     }
 }
