282fda3e8f7066f714d3462a1825febba25f0d0e
lzcd <lzcd@hotmail.com>
2016-03-04 21:21:47 +1100

    Simplistic first step of finding the first matching nodes and relationships

--- Sticky/Cypher/Match.cs
+++ Sticky/Cypher/Match.cs
@@ -32,18 +32,52 @@ namespace Sticky.Cypher
             }
             var connectionDescription = pathCriteria.ConnectionDescriptions.First();
             var relationshipDescription = connectionDescription.RelationshipDescription;
-            switch (relationshipDescription.Direction)
-            {
-                case RelationshipDirection.Left:
 
-                    break;
-                case RelationshipDirection.Right:
-                    break;
-                default:
-                    throw new Exception();
+            foreach (var matchingNode in matchingNodes)
+            {
+                var relationships = default(IEnumerable<Relationship>);
+                switch (relationshipDescription.Direction)
+                {
+                    case RelationshipDirection.Left:
+                        relationships = matchingNode.IncomingRelationships;
+                        break;
+                    case RelationshipDirection.Right:
+                        relationships = matchingNode.OutgoingRelationships;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                var matchingRelationships = new List<Relationship>();
+                foreach (var relationship in relationships)
+                {
+                    if (!DoesMatchDescription(relationship, relationshipDescription))
+                    {
+                        continue;
+                    }
+                    matchingRelationships.Add(relationship);
+                }
+
             }
         }
 
+        private static bool DoesMatchDescription(Relationship relationship, RelationshipMatchDescription description)
+        {
+            if (!description.Labels.Contains(relationship.Label))
+            {
+                return false;
+            }
+
+            foreach (var propertyDescription in description.PropertyDescriptions)
+            {
+                if (!DoesMatchDescription(relationship.PropertyByName, propertyDescription))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
         private static bool DoesMatchDescription(Node node, NodeMatchDescription description)
         {
@@ -52,41 +86,44 @@ namespace Sticky.Cypher
                 return false;
             }
 
-            var unmatchedPropertyFound = false;
             foreach (var propertyDescription in description.PropertyDescriptions)
             {
-                var name = propertyDescription.Name;
-
-
-                var testValue = default(HasValue);
-                if (!node.PropertyByName.TryGetValue(name, out testValue))
+                if (!DoesMatchDescription(node.PropertyByName, propertyDescription))
                 {
                     return false;
                 }
-
-
-                var value = default(HasValue);
-
-                if (propertyDescription.TextValue.StartsWith("'") &&
-                    propertyDescription.TextValue.EndsWith("'"))
-                {
-                    var textValue = propertyDescription.TextValue.Substring(1, propertyDescription.TextValue.Length - 2);
-                    value = new Text { Value = textValue };
-                }
-                else
-                {
-                    var numericValue = default(decimal);
-                    Decimal.TryParse(propertyDescription.TextValue, out numericValue);
-                    value = new Number { Value = numericValue };
-                }
-
-                if (!testValue.Equals(value))
-                {
-                    unmatchedPropertyFound = true;
-                    break;
-                }
             }
-            if (unmatchedPropertyFound)
+  
+            return true;
+        }
+
+       
+        private static bool DoesMatchDescription(Dictionary<string, HasValue> propertyByName, PropertyDescription propertyDescription)
+        {
+            var name = propertyDescription.Name;
+
+            var testValue = default(HasValue);
+            if (!propertyByName.TryGetValue(name, out testValue))
+            {
+                return false;
+            }
+
+            var value = default(HasValue);
+
+            if (propertyDescription.TextValue.StartsWith("'") &&
+                   propertyDescription.TextValue.EndsWith("'"))
+            {
+                var textValue = propertyDescription.TextValue.Substring(1, propertyDescription.TextValue.Length - 2);
+                value = new Text { Value = textValue };
+            }
+            else
+            {
+                var numericValue = default(decimal);
+                Decimal.TryParse(propertyDescription.TextValue, out numericValue);
+                value = new Number { Value = numericValue };
+            }
+
+            if (!testValue.Equals(value))
             {
                 return false;
             }
